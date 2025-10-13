2528addbcfec3b6a10b94eea030aa8c72ceda33c
Rejep <rsemsetdinov@gmail.com>
2025-10-14 04:22:18 +0500

    Delete project project1

--- project1/src/main/java/studying/interfaces/ICarFactory.java
+++
@@ -1,8 +0,0 @@
-package studying.interfaces;
-
-import studying.domains.Car;
-import studying.params.EmptyEngineParams;
-
-public interface ICarFactory<TParams> {
-    Car createCar(TParams carParams, int carNumber);
-}
